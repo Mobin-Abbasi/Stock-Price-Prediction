fd444b1bd0cf938acb470e9aa87bf3b9906841b9
Mobin <mobin04dev@gmail.com>
2024-05-06 20:54:20 +0330

    import libraries

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as np
+import numpy as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
