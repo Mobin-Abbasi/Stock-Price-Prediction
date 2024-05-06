cd92733c47fbc96957e5bb81aece4fcba507654d
Mobin <mobin04dev@gmail.com>
2024-05-06 21:06:14 +0330

    make rando num

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import pandas as np
-import numpy as pd
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
+
+
+np.random.seed(42)
+num_data_points = 100
+days = np.arange(1, num_data_points + 1)
+prices = 100 + np.cumsum(np.random.rand(num_data_points))
