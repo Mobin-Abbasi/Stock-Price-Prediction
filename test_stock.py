efec08472d2a0c3f3fa8f6e7796fa898de02d88a
Mobin <mobin04dev@gmail.com>
2024-05-07 00:06:09 +0330

    finish project

--- test_stock.py
+++ test_stock.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
-import pytest
 from stock import train_model, predict_prices, display_results
 
 
